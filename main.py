fc2537200075bf6c602bba390c20e441ac65ac10
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 16:26:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Giraffe\nAcademy")
+
+# Insert a new line
+
+print("Giraffe\"Academy\"")
+
+# Insert quotation mark
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+# store strings to variable
+
+print(phrase + " is cool")
+
+# add text by + "your word"
+
+# print(phrase.lower()) to convert a string to lowercase
+# print(phrase.upper()) to convert a string to uppercase
+# print(phrase.isupper()) to check string if it's true or false
+# print(phrase.upper.isupper()) combine 2 function
+
+print(len(phrase))
+
+# counting character function
+
+print(phrase[0])
+
+# select one character from string in order from 0 to X
+
+print(phrase.index("G"))
+
+# to show an order of specific letter but start from 0
+
+print(phrase.replace("Giraffe", "Elephant"))
+
+# replace a string
+
+
